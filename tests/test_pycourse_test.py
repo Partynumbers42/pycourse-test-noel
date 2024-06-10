02a93d7f87d0c1a2af7cf65406b0bff092218284
Partynumbers42 <118724611+Partynumbers42@users.noreply.github.com>
2024-06-10 14:34:27 +0200

    added test functions

--- tests/test_pycourse_test.py
+++ tests/test_pycourse_test.py
@@ -1,2 +1,12 @@
+from pycourse_test import algos
+
+def test_add_one():
+    out = algos.add_one(1)
+    assert out == 2, f'output should be 2 but is {out}'
+
+def test_add_two():
+    out = algos.add_two(1)
+    assert out == 3
+
 def test_something():
     pass
