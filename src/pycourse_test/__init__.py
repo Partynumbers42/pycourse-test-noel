60fc79e8509f60aaf29ac5b1d70b71e2096766a4
Partynumbers42 <118724611+Partynumbers42@users.noreply.github.com>
2024-06-10 13:36:43 +0200

    added algros

--- src/pycourse_test/__init__.py
+++ src/pycourse_test/__init__.py
@@ -8,3 +8,5 @@ except PackageNotFoundError:
     __version__ = "uninstalled"
 __author__ = "F. Noel"
 __email__ = "test@test.com"
+
+from . import algos # only then it can be accessed directly from package.modul.function instead of import module from package 
